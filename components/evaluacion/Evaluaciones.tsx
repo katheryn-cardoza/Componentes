d12962751ef536769cab5f24885c3caa17dedf7c
katheryn-cardoza <158368583+katheryn-cardoza@users.noreply.github.com>
2024-02-19 16:19:36 -0600

    Correcciones

--- components/evaluacion/Evaluaciones.tsx
+++ components/evaluacion/Evaluaciones.tsx
@@ -4,45 +4,43 @@ import StarEmpty from "../evaluacion/StartEmpty";
 import StarSemifull from "../evaluacion/StartHalf";
 import StarFilled from "../evaluacion/StartFilled";
 
+
+
 export type EvalProps = {
     calificacion: number;
     onCalificacionChange: any;
 };
 
 export type StarProp = {
-    posicion: number;
-    onClick: any;
+    posicion:number;
+    onClick:any;
+
 };
 
 const Evaluacion = (props: EvalProps) => {
-    const [filledStars, setFilledStars] = useState<number[]>(Array(5).fill(0));
-
-    const handleOnClick = (position: number) => {
-        const newFilledStars = [...filledStars];
-        newFilledStars[position - 1] = (newFilledStars[position - 1] + 1) % 3;
-        setFilledStars(newFilledStars);
-        const newCalificacion = newFilledStars.reduce((acc, cur) => acc + (cur / 2), 0);
-        props.onCalificacionChange(newCalificacion);
-    };
-
+      const handleOnClick = (posicion:number) => {
+        props.onCalificacionChange(posicion+1)
+      }
     return (
-        <View style={styles.contenedor}>
-            {[1, 2, 3, 4, 5].map((posicion) => (
-                <React.Fragment key={posicion}>
-                    {filledStars[posicion - 1] === 0 && <StarEmpty onClick={() => handleOnClick(posicion)} posicion={posicion} />}
-                    {filledStars[posicion - 1] === 1 && <StarSemifull onClick={() => handleOnClick(posicion)} posicion={posicion} />}
-                    {filledStars[posicion - 1] === 2 && <StarFilled onClick={() => handleOnClick(posicion)} posicion={posicion} />}
-                </React.Fragment>
-            ))}
-        </View>
+    <View style ={styles.contenedor}>
+        {props.calificacion == 0 && (<StarEmpty onClick={handleOnClick} posicion={0}/>)}
+        {props.calificacion >= 1 && (<StarFilled onClick={handleOnClick} posicion={0}/>)}
+        {props.calificacion < 2 && (<StarEmpty onClick={handleOnClick} posicion={1}/>)}
+        {props.calificacion >= 2 && (<StarFilled onClick={handleOnClick} posicion={1}/>)}
+        {props.calificacion < 3 && (<StarEmpty onClick={handleOnClick} posicion={2}/>)}
+        {props.calificacion >= 3 && (<StarFilled onClick={handleOnClick} posicion={2}/>)}
+        {props.calificacion < 4 && (<StarEmpty onClick={handleOnClick} posicion={3}/>)}
+        {props.calificacion >= 4 && (<StarFilled onClick={handleOnClick} posicion={3}/>)}
+        {props.calificacion < 5 && (<StarEmpty onClick={handleOnClick} posicion={4}/>)}
+        {props.calificacion >= 5 && (<StarFilled onClick={handleOnClick} posicion={5}/>)}
+         
+    </View>
     );
 };
-
 const styles = StyleSheet.create({
-    contenedor: {
-        display: 'flex',
-        flexDirection: 'row',
-    },
-});
+    contenedor:{
+        display: 'flex', flexDirection:'row',
+    }
+})
 
-export default Evaluacion;
+export default Evaluacion
